a372ee3a8bdf307bb1a9de78cb75f75e531d5d33
DArcy16 <walterluke1612@gmail.com>
2022-11-03 17:56:27 +0630

    update

--- src/components/ProductGroup.jsx
+++ src/components/ProductGroup.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import ProductRow from './ProductRow'
 
-const ProductGroup = ( {product} ) => {
+const ProductGroup = ({product}) => {
   return (
     <div className='mt-4'>
              <div className='flex justify-between text-2xl text-bold'>
                 <h2>Category</h2>
                 <h2>Price</h2>
                </div>
-              <ProductRow name={product.name} price={product.price}/>
+               <div className="collapse">
+                  <input type="checkbox" /> 
+                  
+                    <ProductRow product={product} />
+                  
+            </div>
+              
             </div>
   )
 }
